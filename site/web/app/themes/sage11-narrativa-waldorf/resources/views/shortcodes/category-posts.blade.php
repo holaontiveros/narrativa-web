01af4635d180341bbf376b3c07d3243654cdf7df
Javier Ontiveros <hola@javo.work>
2026-04-16 17:10:08 -0600

    chore: updated shortcodes

--- site/web/app/themes/sage11-narrativa-waldorf/resources/views/shortcodes/category-posts.blade.php
+++ site/web/app/themes/sage11-narrativa-waldorf/resources/views/shortcodes/category-posts.blade.php
@@ -3,15 +3,17 @@
     'query',
 ])
 
-<div class="posts-by-category">
-    @if ($main_title)
-        <h2 class="headline-4 mb-10">{{ $main_title }}</h2>
-    @endif
-    <div class="flex flex-wrap md:flex-nowrap gap-8">
-        @while ($query->have_posts())
-            @php($query->the_post())
-            @include('partials.content')
-        @endwhile
-        @php(wp_reset_postdata())
+@if ($query->have_posts())
+    <div class="posts-by-category">
+        @if ($main_title)
+            <h2 class="headline-4 mb-10">{{ $main_title }}</h2>
+        @endif
+        <div class="flex flex-wrap md:flex-nowrap gap-8">
+            @while ($query->have_posts())
+                @php($query->the_post())
+                @include('partials.content')
+            @endwhile
+            @php(wp_reset_postdata())
+        </div>
     </div>
-</div>
+@endif
