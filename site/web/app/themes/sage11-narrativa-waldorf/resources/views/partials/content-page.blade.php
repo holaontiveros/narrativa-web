b243ef4fec8f26683e4ad1992f2571cef9d38f94
Javier Ontiveros <hola@javo.work>
2026-04-17 09:35:45 -0600

    chore: added styles for non-elementor pages

--- site/web/app/themes/sage11-narrativa-waldorf/resources/views/partials/content-page.blade.php
+++ site/web/app/themes/sage11-narrativa-waldorf/resources/views/partials/content-page.blade.php
@@ -1,7 +1,14 @@
-@php(the_content())
+@if (class_exists('Elementor\Plugin') &&
+        Elementor\Plugin::instance()->documents->get(get_the_ID())->is_built_with_elementor())
+    @php(the_content())
+@else
+    <div class="prose mx-auto max-w-3xl">
+        @php(the_content())
+    </div>
+@endif
 
 @if ($pagination())
-  <nav class="page-nav" aria-label="Page">
-    {!! $pagination !!}
-  </nav>
+    <nav class="page-nav" aria-label="Page">
+        {!! $pagination !!}
+    </nav>
 @endif
