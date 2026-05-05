d5997f6f60e27b83523b14b7a1697ca9ac3f101f
Javier Ontiveros <hola@javo.work>
2026-05-05 10:54:45 -0600

    chore: improve logic for cats

--- site/web/app/themes/sage11-narrativa-waldorf/resources/views/partials/content-single.blade.php
+++ site/web/app/themes/sage11-narrativa-waldorf/resources/views/partials/content-single.blade.php
@@ -1,9 +1,32 @@
-<article @php(post_class('h-entry'))>
+<article
+@php
+post_class('h-entry')
+@endphp
+>
     <div class="prose mx-auto max-w-3xl">
-        @if ($categories = get_the_category())
+        @php
+            $breadcrumb_trail = [];
+
+            if ($categories = get_the_category()) {
+                // Find a category that has a parent (is a sub-category)
+                foreach ($categories as $category) {
+                    if ($category->parent != 0) {
+                        // Build breadcrumb trail from root to current category
+                        $current = $category;
+                        while ($current) {
+                            array_unshift($breadcrumb_trail, $current);
+                            $current = $current->parent ? get_category($current->parent) : null;
+                        }
+                        break;
+                    }
+                }
+            }
+        @endphp
+
+        @if (!empty($breadcrumb_trail))
             <nav class="mb-4 pt-6 not-prose" aria-label="Breadcrumb">
                 <ul class="flex space-x-2 text-sm text-gray-500">
-                    @foreach ($categories as $category)
+                    @foreach ($breadcrumb_trail as $category)
                         <li class="flex items-center space-x-2">
                             <a href="{{ get_category_link($category) }}" @class(['hover:underline', 'text-primary-500' => $loop->last])>
                                 {{ $category->name }}
@@ -18,7 +41,7 @@
         @endif
 
         <header>
-            <h1 class="p-name">
+            <h1 class="p-name mb-4!">
                 {!! $title !!}
             </h1>
 
